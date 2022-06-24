5fa9a6b1389b8e27c0999fc720b8c9bb4c99b500
Bruno Zampieri <brunozampieri1985@gmail.com>
2022-06-24 11:18:17 -0300

    new logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styles from './Navbar.module.css'
-
+import { IoMenuOutline } from 'react-icons/io5'
 
 const Navbar: React.FC = (props) => (
    <nav className={styles.navbar}>
-      <Image src="/logo-rounded.jpg" width={90} height={90} alt="logo" />
+      <Image src="/logo-full.svg" width={280} height={70} alt="logo" />
+      <div className={styles.navbar__menuIcon}>
+         <IoMenuOutline fontSize={24} color={'#fff'} />
+      </div>
    </nav>
 )
 export default Navbar
