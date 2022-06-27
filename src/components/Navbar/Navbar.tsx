ff02ca6f0d131ee1fcae1bcc041c859040ca97e4
Bruno Zampieri <brunozampieri1985@gmail.com>
2022-06-27 12:00:14 -0300

    logos fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,14 +2,18 @@ import Image from 'next/image'
 import styles from './Navbar.module.css'
 import { IoMenuOutline } from 'react-icons/io5'
 import Cart from '@components/Cart'
+import useDeviceWidth from '@hooks/useDevice'
 
-const Navbar: React.FC = () => (
-   <nav className={styles.navbar}>
-      <Image src="/logo-full.svg" width={422} height={80} alt="logo" />
-      <Cart />
-      <div className={styles.navbar__menuIcon}>
-         <IoMenuOutline fontSize={24} color={'#fff'} />
-      </div>
-   </nav>
-)
+const Navbar: React.FC = () => {
+   const deviceWidth = useDeviceWidth()
+   return (
+      <nav className={styles.navbar}>
+         <Image src={deviceWidth > 390 ? "/logo-full.svg" : "/logo-rounded.svg"} width={deviceWidth > 390 ? 300 : 90} height={90} alt="logo" />
+         <Cart />
+         <div className={styles.navbar__menuIcon}>
+            <IoMenuOutline fontSize={24} color={'#fff'} />
+         </div>
+      </nav>
+   )
+}
 export default Navbar
