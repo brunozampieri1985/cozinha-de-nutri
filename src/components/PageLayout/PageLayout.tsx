d55a51c9f454ff0673da635f40c291dab1e2c404
Bruno Zampieri <brunozampieri1985@gmail.com>
2022-07-01 08:19:40 -0300

    feat: social icons

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,17 +1,21 @@
-import Navbar from '@components/Navbar'
+import styles from './PageLayout.module.css'
 import Head from 'next/head'
 import { ReactNode } from 'react'
-import styles from './PageLayout.module.css'
+import Navbar from '@components/Navbar'
+import Socials from '@components/Socials'
 
 const PageLayout: React.FC<{ children: ReactNode }> = ({ children }) => (
    <div className={styles.pageLayout}>
       <Head>
-         <title>Cozinha de Nutri</title>
+         <title>Cozinha de Nutri - Comida de Verdade!</title>
          <meta name="description" content="Marmitas feitas com muito amor." />
          <link rel="icon" href="/favicon.ico" />
       </Head>
       <header><Navbar /></header>
-      <main className={styles.pageContainer}>{children}</main>
+      <main className={styles.pageContainer}>
+         {children}
+         <Socials />
+         </main>
    </div>
 )
 export default PageLayout
---
+++ src/components/Socials/Socials.tsx
@@ -0,0 +1,25 @@
+import styles from './Socials.module.css'
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const Socials: React.FC = () => {
+   const router = useRouter()
+
+   return (
+      <div className={styles.socials}>
+         <div className={styles.whatsapp}>
+            <Link href="https://wa.me/5513991632069" passHref target={'_blank'}>
+               <FaWhatsapp />
+            </Link>
+         </div>
+         <div className={styles.instagram}>
+            <Link href="https://www.instagram.com/cozinha.denutri/">
+               <FaInstagram />
+            </Link>
+         </div>
+      </div>
+   )
+}
+
+export default Socials
